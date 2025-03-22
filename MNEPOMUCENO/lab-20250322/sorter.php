6b78bd3ab0eb220701f82a61d9c502556defd7b6
Mary Rose Princess Nepomuceno <mmvpnepomuceno.com>
2025-03-22 13:09:51 +0800

    Added resort.php file

---
+++ MNEPOMUCENO/lab-20250322/sorter.php
@@ -0,0 +1,26 @@
+<?php
+
+$numbers = [];
+
+echo "please enter 10 different numbers:\n";
+
+for ($i = 0; $i < 10; $i++) {
+    echo "enter number ". ($i+1) . ":";
+    $input = trim (fgets(STDIN));
+    if (is_numeric(input)) {
+        $numbers[]= (float)$input;
+    } else {
+        echo "invalid input. Please enter a number. \n";
+        $i--;
+    }
+}
+
+sort($numbers);
+
+echo "\Sorted order of the entered numbers:\n";
+
+foreach ($numbers as $number) {
+    echo $number . " ";
+}
+echo "\n";
+?>

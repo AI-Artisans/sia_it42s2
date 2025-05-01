c3ea7b67fd4f590c73af7bc107bbb9d8fa92819a
Ryuhiro <mryuhiro@gmail.com>
2025-05-01 18:22:58 +0800

    Updated/Organized Files.

---
+++ MRRosas/Lab-20250426/application/controllers/Activities.php
@@ -0,0 +1,14 @@
+<?php
+
+class Activities extends CI_Controller {
+	public function construct() {
+		parent::_construct();
+	}
+
+	public function index() {
+		$this->load->model('Blogs_model', 'blogs');
+		$data['query'] = $this->blogs->getSome(5);
+
+		$this->load->view('msgview', $data);
+	}
+}
